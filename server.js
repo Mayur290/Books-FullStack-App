d6588a2820b1f1cc477b320d48922407ce180420
Mayur290 <mayurbansal98@gmail.com>
2021-04-02 18:14:59 +0530

    BAsic Setup Done

---
+++ server.js
@@ -0,0 +1,31 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const express = require("express");
+const app = express();
+const expressLayouts = require("express-ejs-layouts");
+const { success, error } = require("consola");
+const PORT = process.env.PORT || 3000;
+
+const indexRouter = require("./routes/index");
+
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout");
+app.use(expressLayouts);
+app.use(express.static("public"));
+
+app.use("/", indexRouter);
+
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", (error) => console.log(error));
+db.once("open", () => {
+  success({ message: `Connected to Mongodb Database`, badge: true });
+});
+
+app.listen(PORT, () => {
+  success({ message: `Server started on port ${PORT}`, badge: true });
+});
